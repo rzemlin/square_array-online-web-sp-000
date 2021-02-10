7926d8b8542f4d87d1069e64f2606d9d517e220c
Ryan Zemlin <rzemlin@gmail.com>
2021-02-10 14:25:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
-   array.each do |num|
-  new_list_numbers = num**2
-  puts "#{new_list_numbers}"
-  new_list = [new_list_numbers]
+  new_list = []
+   array.each do {|num| new_list << num**2}
+  new_list_numbers
   end
 end
